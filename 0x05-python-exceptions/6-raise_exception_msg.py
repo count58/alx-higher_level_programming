ae14786127d953fec201523fddb4cd06c2da3d72
chuwa peter <chuwapeter@yahoo.com>
2023-07-14 12:09:47 -0700

    function that raises a name exception with a message.

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 
-def raise_exception():
-    """Raise a TypeError exception."""
-    raise TypeError
+def raise_exception_msg(message=""):
+    """Raise a NameError exception with a message."""
+    raise NameError(message)
