34d667f8f8c033a19e0bf7e6e9e73b575ef19bb3
chuwa peter <chuwapeter@yahoo.com>
2023-09-13 12:33:37 -0700

    javascript objects scopes closures

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -3,4 +3,4 @@ const fs = require('fs');
 
 const fArg = fs.readFileSync(process.argv[2]).toString();
 const sArg = fs.readFileSync(process.argv[3]).toString();
-fs.writeFileSync(process.argv[4], fArg + sArg)
+fs.writeFileSync(process.argv[4], fArg + sArg);
