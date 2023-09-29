28f0b3e248afb6057e18dc8426db3074012d7fb5
chuwa peter <chuwapeter@yahoo.com>
2023-09-29 05:29:22 -0700

    0x11 Python Network 1

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -4,26 +4,12 @@
 Usage: ./10-my_github.py <GitHub username> <GitHub password>
   - Uses Basic Authentication to access the ID.
 """
-import requests
 import sys
+import requests
+from requests.auth import HTTPBasicAuth
 
-if len(sys.argv) != 3:
-    print("Usage: python script.py <username> <personal_access_token>")
-    sys.exit(1)
 
-username = sys.argv[1]
-personal_access_token = sys.argv[2]
-
-url = "https://api.github.com/user"
-
-try:
-    response = requests.get(url, auth=(username, personal_access_token))
-    response.raise_for_status()
-
-    user_info = response.json()
-    user_id = user_info.get('id')
-
-    print(f"Your GitHub user ID: {user_id}")
-
-except requests.exceptions.RequestException as e:
-    print(f"Error fetching GitHub user information: {e}")
+if __name__ == "__main__":
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    r = requests.get("https://api.github.com/user", auth=auth)
+    print(r.json().get("id"))
--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,24 +1,14 @@
 #!/usr/bin/python3
 """ script that takes in a URL, sends a request to
  the URL and displays the body of the response."""
- import requests
 import sys
 
-if len(sys.argv) != 2:
-    print("Usage: python script.py <URL>")
-    sys.exit(1)
+import requests
 
-url = sys.argv[1]
-
-try:
-    response = requests.get(url)
-    response.raise_for_status()
-
-    content = response.text
-    print(content)
-
-    if response.status_code >= 400:
-        print(f"Error code: {response.status_code}")
-
-except requests.exceptions.RequestException as e:
-    print(f"Error fetching URL: {e}")
+if __name__ == "__main__":
+    url = sys.argv[1]
+    r = requests.get(url)
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print(r.text)
