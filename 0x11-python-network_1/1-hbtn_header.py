fa564bf98878f03a4df1b81232f14c35c994b895
chuwa peter <chuwapeter@yahoo.com>
2023-09-29 04:52:11 -0700

    0x11 Python Network 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
-#takes in a URL, sends a request to the URL and displays the value of the X-Request-Id variable found in the header of the response.Usage: ./1-hbtn_header.py <URL>
-import urllib.request
+"""takes in a URL, sends a request to the URL and displays the
+value of the X-Request-Id variable found in the header of the response.
+
+Usage: ./1-hbtn_header.py <URL>
+"""
 import sys
 
-if len(sys.argv) != 2:
-    print("Usage: python script.py <URL>")
-    sys.exit(1)
+import urllib.request
 
-url = sys.argv[1]
+if __name__ == "__main__":
+    url = sys.argv[1]
 
-try:
-    with urllib.request.urlopen(url) as response:
-        x_request_id = response.getheader('X-Request-Id')
-        print(x_request_id)
-except urllib.error.URLError as e:
-    print(f"Error fetching URL: {e}")
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as response:
+        print(dict(response.headers).get("X-Request-Id"))
